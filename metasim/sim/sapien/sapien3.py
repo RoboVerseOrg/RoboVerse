0ed828036450d9cb775544c044e0904a6456ba6a
Feishi Wang <feishiwang2001@gmail.com>
2025-04-15 16:25:02 -0700

    add sort argument to get_joint_names()

--- metasim/sim/sapien/sapien3.py
+++ metasim/sim/sapien/sapien3.py
@@ -467,9 +467,12 @@ class SingleSapien3Handler(BaseSimHandler):
     def device(self) -> torch.device:
         return torch.device("cpu")
 
-    def get_joint_names(self, obj_name: str) -> list[str]:
+    def get_joint_names(self, obj_name: str, sort: bool = True) -> list[str]:
         if isinstance(self.object_dict[obj_name], ArticulationObjCfg):
-            return self.object_joint_order[obj_name]
+            joint_names = self.object_joint_order[obj_name]
+            if sort:
+                joint_names.sort()
+            return joint_names
         else:
             return []
 
