5852f415e6fc4c69456187fd032c5c6f5890de65
weisonweileen <weisonweileen@gmail.com>
2025-05-10 22:00:21 +0800

    update annotation
    Signed-off-by: weisonweileen <weisonweileen@gmail.com>

--- metasim/cfg/control.py
+++ metasim/cfg/control.py
@@ -7,11 +7,11 @@ from metasim.utils import configclass
 
 @configclass
 class ControlCfg:
-    """high level control config parameters cfg.
+    """high level control parameters cfg.
 
-    This class defines the parameters for the control configuration. Note that stiffness and damping of each joint's joints are configured at robot cfg.
+    This class defines the higher level for the control configuration.
     """
 
-    action_scale: float = 1.0
+    action_scale: float = 1.0 # to scale the actions
     torque_limit_scale: float = 1.0  # scale it down can ensure safety
     action_offset: bool = False  # set true if target position = action * action_scale + default position
