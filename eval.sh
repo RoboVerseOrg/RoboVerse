a7a8511f8622a6068043a9907028563cd5e093a6
WinterMelooooo <2538358797@qq.com>
2025-05-18 03:29:57 +0000

    Ready for PR

--- eval.sh
+++
@@ -1,5 +0,0 @@
-python roboverse_learn/eval.py --task CloseBox --algo diffusion_policy --num_envs 50 --wrapper_class pnt_cloud_wrapper --checkpoint_path /home/yktang/Repos/RoboVerse_dev/info/outputs/2025.04.15/05.55.33_CloseBox_franka_level0_100_obs:ee_act:ee/checkpoints/200.ckpt
-export WANDB_API_KEY=263d7ff027d2057a793fb7f51783d43f5b6344cc
-export CUDA_VISIBLE_DEVICES=0
-python roboverse_learn/eval.py --task CloseBox --algo diffusion_policy --max_step 500 --num_envs 1 --task_id_range_low 90 --task_id_range_high 100 --random.level 1 --headless --checkpoint_path /home/ghr/yktang/RoboVerse/info/outputs/DP/2025.05.13/17.11.05_RGBD_MultiViT_CloseBoxFrankaL1_obs:joint_pos_act:joint_pos/checkpoints/2000.ckpt
-#python roboverse_learn/eval.py --task StackCube --algo diffusion_policy --max_step 500 --num_envs 50 --task_id_range_low 0 --task_id_range_high 100 --random.level 0 --headless --checkpoint_path /home/ghr/yktang/RoboVerse/info/outputs/DP/2025.05.10/19.32.51_rgbd_vit_StackCubeFrankaL0_obs:joint_pos_act:joint_pos/checkpoints/2000.ckpt
