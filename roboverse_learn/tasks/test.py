fe72e167206a776ad81e593f2a7a195b238d98ee
boshi-an <boshi_an@126.com>
2025-07-24 00:11:11 +0800

    refined test script

--- roboverse_learn/tasks/test.py
+++ roboverse_learn/tasks/test.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     scenario = ScenarioCfg(
         robots=["franka"],
         try_add_table=False,
-        sim="pybullet",
-        headless=False,
+        sim="mujoco",
+        headless=True,
         num_envs=1,
     )
 
@@ -39,16 +39,16 @@ if __name__ == "__main__":
             name="bbq_sauce",
             scale=(2, 2, 2),
             physics=PhysicStateType.RIGIDBODY,
-            usd_path="../get_started/example_assets/bbq_sauce/usd/bbq_sauce.usd",
-            urdf_path="../get_started/example_assets/bbq_sauce/urdf/bbq_sauce.urdf",
-            mjcf_path="../get_started/example_assets/bbq_sauce/mjcf/bbq_sauce.xml",
+            usd_path="../../get_started/example_assets/bbq_sauce/usd/bbq_sauce.usd",
+            urdf_path="../../get_started/example_assets/bbq_sauce/urdf/bbq_sauce.urdf",
+            mjcf_path="../../get_started/example_assets/bbq_sauce/mjcf/bbq_sauce.xml",
         ),
         ArticulationObjCfg(
             name="box_base",
             fix_base_link=True,
-            usd_path="../get_started/example_assets/box_base/usd/box_base.usd",
-            urdf_path="../get_started/example_assets/box_base/urdf/box_base_unique.urdf",
-            mjcf_path="../get_started/example_assets/box_base/mjcf/box_base_unique.mjcf",
+            usd_path="../../get_started/example_assets/box_base/usd/box_base.usd",
+            urdf_path="../../get_started/example_assets/box_base/urdf/box_base_unique.urdf",
+            mjcf_path="../../get_started/example_assets/box_base/mjcf/box_base_unique.mjcf",
         ),
     ]
 
