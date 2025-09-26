e8271bf859143bd2fddf6b631d6b977909d3444c
MurphyZhao04 <xh930717304@gmail.com>
2025-09-25 20:23:43 -0700

    [feat,doc] user-friendly reconstruct

--- get_started/rl/fast_td3/1_fttd3.py
+++ get_started/rl/fast_td3/1_fttd3.py
@@ -122,6 +122,7 @@ from torch.amp import GradScaler, autocast
 from get_started.rl.fast_td3.fttd3_module import Actor, Critic, EmpiricalNormalization, SimpleReplayBuffer
 from metasim.scenario.cameras import PinholeCameraCfg
 from metasim.task.registry import get_task_class
+from metasim.utils.obs_utils import ObsSaver
 
 
 def main() -> None:
@@ -304,22 +305,27 @@ def main() -> None:
 
         obs_normalizer.eval()
         obs, info = env.reset()
-        frames = [env.render()]
+
+        # Initialize video recording
+        os.makedirs("get_started/output/rl", exist_ok=True)
+        obs_saver = ObsSaver(video_path=f"get_started/output/rl/1_fttd3_{simulator}.mp4")
+        obs_orin = env.handler.get_states()
+        obs_saver.add(obs_orin)
 
         for _ in range(env.max_episode_steps):
             with torch.no_grad(), autocast(device_type=amp_device_type, dtype=amp_dtype, enabled=amp_enabled):
                 actions = actor(obs_normalizer(obs))
             obs, _, done, _, _ = env.step(actions.float())
 
-            frames.append(env.render())
+            obs_orin = env.handler.get_states()
+            obs_saver.add(obs_orin)
             if done.any():
                 break
 
+        obs_saver.save()
         env.close()
         obs_normalizer.train()
 
-        iio.mimsave(video_path, frames, fps=30)
-        return frames
 
     def update_main(data, logs_dict):
         with autocast(device_type=amp_device_type, dtype=amp_dtype, enabled=amp_enabled):
