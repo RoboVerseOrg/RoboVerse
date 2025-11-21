a7bfbd559d3b3020444a03c31d4588207e3e6099
Fisher Wang <feishiwang2001@gmail.com>
2025-11-21 13:34:54 -0800

    [Autotest] optimize isaacsim launching time (#682)
    * [update] work for isaacsim only
    
    * [update] work for mujoco-only case too, but have to launch isaacsim
    
    * [update] restructure code
    
    * [fix] isaacsim headless mode
    
    * polish
    
    * polish
    
    ---------
    
    Co-authored-by: weison_pan <124496507+WeisonWEileen@users.noreply.github.com>

--- metasim/test/test_utils.py
+++ metasim/test/test_utils.py
@@ -1,6 +1,8 @@
 import math
 
+import pytest
 import torch
+from loguru import logger as log
 
 
 def assert_close(a, b, atol=1e-3, message="Consistency Error"):
@@ -26,12 +28,57 @@ def get_test_parameters():
     mjx_params = [("mjx", 1)]
     sapien3_params = [("sapien3", 1)]
     sapien2_params = [("sapien2", 1)]
-    return (
-        mujoco_params
-        + mjx_params
-        + isaacsim_params
-        + isaacgym_params
-        + genesis_params
-        + sapien3_params
-        + sapien2_params
-    )
+    return mujoco_params + isaacsim_params + isaacgym_params + genesis_params + sapien3_params + sapien2_params
+
+
+@pytest.fixture(scope="session")
+def isaacsim_app(request):
+    """
+    Create an IsaacSim app if any test case in the session uses "isaacsim". Otherwise, do nothing.
+    """
+    needs_isaacsim = False
+
+    # Get the session object to access collected tests
+    session = request.session
+    if hasattr(session, "items"):
+        for item in session.items:
+            # Check if this test has callspec (parametrized) and uses "isaacsim"
+            if hasattr(item, "callspec") and "sim" in item.callspec.params:
+                if item.callspec.params["sim"] == "isaacsim":
+                    needs_isaacsim = True
+                    break
+
+    if needs_isaacsim:
+        from isaaclab.app import AppLauncher
+
+        app = AppLauncher(headless=True, enable_cameras=True).app
+        yield app
+        # NOTE: Don't call app.close(), otherwise pytest summary will be skipped!
+    else:
+        yield None
+
+
+@pytest.fixture(scope="function", autouse=True)
+def isaacsim_context(request):
+    """
+    Create a new IsaacSim stage if the current test case uses "isaacsim". Otherwise, do nothing.
+    """
+    sim = request.node.callspec.params.get("sim")
+
+    if sim == "isaacsim":
+        import isaaclab.sim as sim_utils
+        import isaacsim.core.utils.stage as stage_utils
+
+        log.debug("Creating new stage")
+        stage_utils.create_new_stage()
+        log.debug("New stage created")
+        sim_cfg = sim_utils.SimulationCfg()  # TODO: pass parameters from scenario cfg
+        sim_context = sim_utils.SimulationContext(sim_cfg)
+        sim_context._app_control_on_stop_handle = None
+        yield sim_context
+        log.debug("Clearing simulation context")
+        sim_context.clear_all_callbacks()
+        sim_context.clear_instance()
+        log.debug("Simulation context cleared")
+    else:
+        yield None
