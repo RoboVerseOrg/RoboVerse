4365ce9ec2a0374e58c39d20a68b062ea23f0c6d
JIAjindou <jdjiabuaa@126.com>
2025-12-03 12:44:52 +0800

    [fix] ckpt_path in dp_runner

--- roboverse_learn/il/dp/runner/dp_runner.py
+++ roboverse_learn/il/dp/runner/dp_runner.py
@@ -548,7 +548,7 @@ class DPRunner(BaseRunner):
         time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         checkpoint = self.get_checkpoint_path()
         # checkpoint = ckpt_path if checkpoint is None else checkpoint
-        checkpoint = ckpt_path if ckpt_path is None else checkpoint
+        checkpoint = ckpt_path if ckpt_path is not None else checkpoint
         if checkpoint is None:
             raise ValueError(
                 "No checkpoint found, please provide a valid checkpoint path."
