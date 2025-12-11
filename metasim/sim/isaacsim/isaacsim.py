4030e097c46e227f94211f313498104973afbbd3
myuansun <1273414643@qq.com>
2025-12-11 22:35:40 +0800

    [fix] replace self.sim.reset() with more accurate sensor init

--- metasim/sim/isaacsim/isaacsim.py
+++ metasim/sim/isaacsim/isaacsim.py
@@ -208,7 +208,9 @@ class IsaacsimHandler(BaseSimHandler):
         # Initialize GS background if enabled
         self._build_gs_background()
         super().launch()
-        self.sim.reset()  # crucial for calling _initialize_callbacks in binded sensors
+        for sensor in self.scene.sensors.values():
+            if hasattr(sensor, "_initialize_callback"):
+                sensor._initialize_callback(None)
 
     def close(self) -> None:
         log.info("close Isaacsim Handler")
