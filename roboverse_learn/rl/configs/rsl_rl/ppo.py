14b5ebf3e4c8649a317b243bf5aa720c789739f2
wangbei <wangbei@agibot.com>
2025-12-26 14:39:21 +0800

    fix:1.Modify the model training path2.Fix device settings and model weights loading in the eval file

--- roboverse_learn/rl/configs/rsl_rl/ppo.py
+++ roboverse_learn/rl/configs/rsl_rl/ppo.py
@@ -97,8 +97,8 @@ class RslRlPPOConfig(RslRlOnPolicyRunnerCfg):
 
         if self.model_dir is None:
             name = self.exp_name or self.experiment_name
-            log_dir = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            self.model_dir = os.path.join("outputs", name, self.task, log_dir)
+            # self.model_dir = os.path.join("outputs", name, self.task)
+            self.model_dir = os.path.join("outputs", name, self.task,datetime.now().strftime("%Y%m%d_%H%M%S"))
 
         if self.obs_groups is None:
             self.obs_groups = {"policy": ["policy"], "critic": ["policy", "critic"]}
